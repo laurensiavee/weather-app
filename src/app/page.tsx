f669546e6b12943bad0fd23f0a34ee295f76d25f
laurensiavee <92752405+laurensiavee@users.noreply.github.com>
2024-04-10 00:35:36 +0700

    remove search bar on page.tsx, create SearchCity component instead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,14 @@
-'use client'
-
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
+import SearchCity from "./component/SearchCity";
 
 export default function Home() {
-  const [city, setCity] = useState("")
-
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const router = useRouter();
-
-  const preventDefault = f => e => {
-    e.preventDefault()
-    f(e)
-  }
-  const handleSubmit = preventDefault(() => {
-    router.push("weather/" + city,)
-  })
-
-
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input
-            onChange={(e) => setCity(e.target.value)}
-            className='' type='text' placeholder='city name'
-            value= {city}
-          />
-        </div>
-        <Link  href={`/weather/${city}`}>
-          search
-        </Link>
-      </form>
+      <SearchCity />
     </>
   );
 }
